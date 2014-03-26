3a4dbd91a9e477779c09e083487119c2057376d8
f3rland <f3rland@gmail.com>
2014-03-26 16:40:53 -0400

    Package updated to use git module https://github.com/guillaumepotier/Parsley.js.git

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 Parsley.js
 ==========
 
-Meteor packaging for Parsley.js.
+Meteor packaging for [Parsley.js](http://parsleyjs.org/doc/index.html).
+Updated with Parsley.js v2.0.0-rc5
 
-If I forgot to upgrade to the latest Parsley version, please start a new issue
-or find a way to notify me and I'll happily upgrade it for you. Or if you want
-to be nice and save me a few seconds; fork it, make a PR and I'll merge it.
-
+``` sh
+$ meteor add parsleyjs2
+```
--- package.js
+++ package.js
@@ -3,8 +3,18 @@ Package.describe({
 });
 
 Package.on_use(function (api) {
-  api.add_files('parsley.min.js', 'client');
-  api.add_files('parsley.es.min.js', 'client');
-  api.add_files('parsley.extend.min.js', 'client');
+	api.use('jquery', ['client']);
+
+	var path = Npm.require('path');
+	var asset_path = path.join('parsley');
+	
+	api.add_files(path.join(asset_path, 'dist', 'parsley.min.js'), 'client');
+	api.add_files(path.join(asset_path, 'dist', 'parsley.remote.min.js'), 'client');
+	api.add_files(path.join(asset_path, 'src', 'extra', 'dateiso.js'), 'client');
+	api.add_files(path.join(asset_path, 'src', 'i18n', 'en.js'), 'client');
+	api.add_files(path.join(asset_path, 'src', 'i18n', 'en.extra.js'), 'client');
+	api.add_files(path.join(asset_path, 'src', 'i18n', 'fr.js'), 'client');
+	api.add_files(path.join(asset_path, 'src', 'i18n', 'fr.extra.js'), 'client');
+	
 });
 
